69a74fab5264a2d83403c7298cf5ee4eeb98887c
cyanfhine <yifan03.chen@tum.de>
2022-11-22 17:49:17 +0100

    thumbnails = new Picture[0]; entfernt

--- src/pgdp/pingunetwork/Picture.java
+++ src/pgdp/pingunetwork/Picture.java
@@ -16,10 +16,10 @@ public class Picture {
         this.location = location;
         this.data = data;
 
-        width = data[0].length;
-        height = data.length;
+        this.width = data[0].length;
+        this.height = data.length;
 
-        thumbnails = new Picture[0];
+
     }
 
     public String getLocation() {
